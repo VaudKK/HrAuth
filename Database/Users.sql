8d988aaadb6432aa532f42c3dec2df45c0f8981a
GlennOnyango <glennonyango@yahoo.com>
2021-07-23 12:11:23 +0300

    Contains database files user

--- Database/Users.sql
+++ Database/Users.sql
@@ -19,3 +19,22 @@
     );
     CREATE INDEX idx_users_id ON users(user_id);
     CREATE INDEX idx_company_id ON users(company_id);
+
+    --Role is created as a controller
+    CREATE TABLE roles(
+        role_id serial PRIMARY key,
+        role_name VARCHAR(100) NOT NULL,
+        description TEXT
+    );
+
+--When the user logs in and creates their profile
+    CREATE TABLE users_profie(
+
+
+        profile_id serial PRIMARY key,
+        user_id REFERENCES users,
+
+        role_id REFERENCES roles NOT NULL,
+
+
+    );
